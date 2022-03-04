206b2044d9337baa0de1a5d4504e8351000f3d7e
TharinduDR <rhtdranasinghe@gmail.com>
2022-03-04 12:10:10 +0000

    001: Initial Commit

--- examples/sentence_level/wmt_2020/ro_en/quest5.py
+++ examples/sentence_level/wmt_2020/ro_en/quest5.py
@@ -60,7 +60,6 @@ for i in range(quest5_config["n_fold"]):
                         use_cuda=torch.cuda.is_available(), args=quest5_config)
 
     preds = model.predict(to_predict)
-    print(preds)
     dev_preds[:, i] = [float(p) for p in preds]
 
 dev['predictions'] = dev_preds.mean(axis=1)
